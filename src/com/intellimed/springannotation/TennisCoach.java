5b3b7b25a04c582ecdc224c06e40b2a39f6702db
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-05-09 15:20:22 -0400

    Inject WeatherService implementation via setters

--- src/com/intellimed/springannotation/TennisCoach.java
+++ src/com/intellimed/springannotation/TennisCoach.java
@@ -10,11 +10,37 @@ public class TennisCoach implements Coach {
 	
 	private WeatherService weatherService;
 
+	public TennisCoach(){
+		System.out.println(">> TennisCoach: Inside TennisCoach()");
+	}
+	
+	/*
 	@Autowired
 	// Constructor injection
 	public TennisCoach(WeatherService weatherService){
 		this.weatherService = weatherService;
 	}
+	*/
+	
+	public WeatherService getWeatherService() {
+		return weatherService;
+	}
+
+	@Autowired
+	// Setter injection
+	public void setWeatherService(WeatherService weatherService) {
+		System.out.println(">> TennisCoach: Inside setWeatherService(WeatherService weatherService)");
+		this.weatherService = weatherService;
+	}
+	
+	/*
+	@Autowired
+	// Any method with any name can be injected!
+	public void doSomeInterestingStuff(WeatherService weatherService) {
+		System.out.println(">> TrackCoach: Inside doSomeInterestingStuff(WeatherService weatherService)");
+		this.weatherService = weatherService;
+	}
+	 */
 	
 	@Override
 	public String getDailyWorkout() {
--- src/com/intellimed/springannotation/TrackCoach.java
+++ src/com/intellimed/springannotation/TrackCoach.java
@@ -8,11 +8,38 @@ public class TrackCoach implements Coach {
 
 	private WeatherService weatherService;
 	
+	public TrackCoach(){
+		System.out.println(">> TrackCoach: Inside TrackCoach()");
+	}
+	
+	/*
 	@Autowired
-	//Constructor injection
+	// Constructor injection
 	public TrackCoach(WeatherService weatherService){
 		this.weatherService = weatherService;
 	}
+	*/
+	
+	public WeatherService getWeatherService() {
+		return weatherService;
+	}
+
+	
+	@Autowired
+	// Setter injection
+	public void setWeatherService(WeatherService weatherService) {
+		System.out.println(">> TrackCoach: Inside setWeatherService(WeatherService weatherService)");
+		this.weatherService = weatherService;
+	}
+	
+	/*
+	@Autowired
+	// Any method with any name can be injected!
+	public void doSomeInterestingStuff(WeatherService weatherService) {
+		System.out.println(">> TrackCoach: Inside doSomeInterestingStuff(WeatherService weatherService)");
+		this.weatherService = weatherService;
+	}
+	 */
 	
 	@Override
 	public String getDailyWorkout() {
