3aff47bb138a65d9c1d7048730a8ef07f455bd72
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-05-25 17:20:47 -0400

    Match aspect with addAccount method in AccountDao only

--- src/com/intellimed/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/intellimed/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-	@Before("execution(public void addAccount())")
+	//@Before("execution(public void addAccount())")
+	@Before("execution(public void com.intellimed.aopdemo.dao.AccountDao.addAccount())")
 	public void beforeAddAccountAdvice(){
 		System.out.println("\n===> Running an @Before advice on methode: public void addAccount()");
 	}
