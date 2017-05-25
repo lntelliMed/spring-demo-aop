fc5fcc44cc6194ee827fa719f71f420b42ff7d21
Sam Alsmadi <Sam.Alsmadi@gmail.com>
2017-05-25 17:13:39 -0400

    Add Membership DAO

--- src/com/intellimed/aopdemo/MainDemoApp.java
+++ src/com/intellimed/aopdemo/MainDemoApp.java
@@ -3,16 +3,20 @@ package com.intellimed.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.intellimed.aopdemo.dao.AccountDao;
+import com.intellimed.aopdemo.dao.MembershipDao;
 
 public class MainDemoApp {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
 		AccountDao accountDao = context.getBean("accountDao", AccountDao.class);
+		MembershipDao membershipDao = context.getBean("membershipDao", MembershipDao.class);
+
 		accountDao.addAccount();
+		membershipDao.addAccount();
 		
-		System.out.println("\n\nRunning addAccount method again!");
-		accountDao.addAccount();
+		//System.out.println("\n\nRunning addAccount method again!");
+		//accountDao.addAccount();
 
 		context.close();
 		
